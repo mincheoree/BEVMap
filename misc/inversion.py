6d1a417ac435b85cddc0847b94729cdeb6e54837
mincheoree <m1ncheoree@naver.com>
2023-12-02 02:23:12 +0000

    augmentation2

--- misc/inversion.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image 
-import PIL.ImageOps    
-
-image = Image.open('./0826/ca9a282c9e77460f8360f564131a8af5.jpg')
-
-inverted_image = PIL.ImageOps.invert(image)
-
-inverted_image.save('tmp.jpg')
